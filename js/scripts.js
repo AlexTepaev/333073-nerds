b4600c6974e164f86c1f34160d2d386e36b87025
Alexander <tepaevalex@gmail.com>
2017-12-13 21:59:23 +0300

    13.12.2017

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,43 @@
 
-            var link = document.querySelector(".btn__map");
+			var link = document.querySelector(".btn__map");
 
 			var popup = document.querySelector(".modal");
 			var close = popup.querySelector(".modal__close");
-			
 			var storage = localStorage.getItem("name");
 
-			var input = document.querySelector("input#name")
+			var input = document.querySelector("input#name");
 			var submit = document.querySelector("button.btn__feedback");
 
 
-        	submit.addEventListener("click", (e) => {
-                e.preventDefault();
-            	if(input.value === "") {
-        		input.classList.add("apply__shake");
-
-    		}    
-		});
-
-		
+			submit.addEventListener("click", function (evt) {
+				e.preventDefault();
+					if(input.value === "") {
+				input.classList.add("apply__shake");
+				}
+			});
 
 
 			link.addEventListener("click", function (evt) {
 			evt.preventDefault();
 			popup.classList.add("modal__show");
-
 			if (storage) {
-      			name.value = storage;
-      			email.focus();
-    		} else {
-    		name.focus();
+				name.value = storage;
+				email.focus();
+			} else {
+			name.focus();
+			}
+		});
+
+			close.addEventListener("click", function (evt) {
+			evt.preventDefault();
+			popup.classList.remove("modal__show");
+			})
+
+			window.addEventListener("keydown", function (evt) {
+					if (evt.keyCode === 27) {
+						if (popup.classList.contains("modal__show")) {
+					 popup.classList.remove("modal__show");
+					}
     		}
-		    });
-
-	        close.addEventListener("click", function (evt) {
-	        evt.preventDefault();
-	        popup.classList.remove("modal__show");
-	      
-	        })
-
-	    
-    		
-
-    		window.addEventListener("keydown", function (evt) {
-    				if (evt.keyCode === 27) {
-      				if (popup.classList.contains("modal__show")) {
-       			 popup.classList.remove("modal__show");
-      			}
-    		}
-  			});
+  		});
 
